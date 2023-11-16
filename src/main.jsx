3b102e59e9dbdab33f6c36e75c789377830dcfba
kalamnsi10 <123523157+kalamansi10@users.noreply.github.com>
2023-11-16 17:40:44 +0800

    Imported CSS from green-mart

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './assets/normalize.css'
+import './assets/layouts.css'
+import './assets/components.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
