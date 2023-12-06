1f77c03c76c3703dddc6929e544bb00a49e9da16
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-06 16:08:23 +0800

    Added fetch for checkout items

--- src/src/checkoutpage/CheckOutPage.jsx
+++ src/src/checkoutpage/CheckOutPage.jsx
@@ -1,5 +1,15 @@
-export default function CheckOutPage() {
+import { useState, useEffect } from 'react'
+export default function CheckOutPage({currentUser}) {
+
+  function getCart() {
+    fetch('api/v1/get-cart', {
+      credentials: 'include',
+    })
+      .then(response => response.json())
+      .then(data => data.filter(item => item.is_for_checkout == false))
+  }
   return (
-    <div>CheckOutPage</div>
+    <>
+    </>
   )
 }
