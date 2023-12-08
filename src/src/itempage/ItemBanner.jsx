a07f81c32417bd2ae4a6cb975bd38e305a265dcb
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-08 16:21:00 +0800

    Added checkout function and input amount controller

--- src/src/itempage/ItemBanner.jsx
+++ src/src/itempage/ItemBanner.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import useItemsProps from '../hooks/useItemsProps'
 import useInput from '../hooks/useInput'
 import AddToCart from './AddToCart'
 
 export default function ItemBanner({ item }) {
-  const [amount, setAmount] = useInput('number')
+  const [amount, inputAmount, setAmount] = useInput('number', '')
   const properties = JSON.parse(useItemsProps())
+
+  useEffect(() => setAmount(1), [])
 
   function mapSpecs() {
     return Object.keys(properties).map(specLabel =>
@@ -22,6 +25,13 @@ export default function ItemBanner({ item }) {
       result = result + word.charAt(0).toUpperCase() + word.slice(1) + ' '
     })
     return result.slice(0, -1)
+  }
+
+  function handleClickAmount(adjustment) {
+    setAmount(prev => {
+      let result = adjustment + prev
+      return result < 0 || result > 999 ? prev : result
+    })
   }
 
   return (
@@ -46,14 +56,14 @@ export default function ItemBanner({ item }) {
       <div>
         <h3>Amount:</h3>
         <div className='flex-row'>
-          <button>-</button>
-          {setAmount}
-          <button>+</button>
+          <button onClick={() => handleClickAmount(-1)}>-</button>
+          {inputAmount}
+          <button onClick={() => handleClickAmount(1)}>+</button>
         </div>
       </div>
       <div>
         <AddToCart item={item} amount={amount}/>
-        <button>Buy now</button>
+        <Link to='/checkout' state={{ from: 'itempage', item: item, amount: amount}}><button>Buy now</button></Link>
       </div>
       <div className='specsContainer'>
         {mapSpecs()}
