f60b824a2e62cdb26e2c98845154dbdedac6c0a2
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-12 12:29:24 +0800

    Added Categories component

--- src/src/homepage/Categories.jsx
+++ src/src/homepage/Categories.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import useItemsProps from '../hooks/useItemsProps'
 
 export default function Categories() {
@@ -6,16 +6,24 @@ export default function Categories() {
 
   function getItem() {
     fetch('/api/v1/get-item/' + id)
-    .then(response => response.json())
+      .then(response => response.json())
+
   }
 
-  function mapCategories() {
-    properties[Object.keys(properties)[0]].map((category) => {
-
+  function renderCategories() {
+    return properties[Object.keys(properties)[0]].map((category) => {
+      return (
+        <div className='category flex-column align-center' key={category}>
+          <img src="https://placehold.co/200x200" alt="placeholder" />
+          <h5>{category}</h5>
+        </div>
+      )
     })
   }
 
   return (
-    <div></div>
+    <div className='categories-container flex-row justify-center'>
+      {renderCategories()}
+    </div>
   )
 }
