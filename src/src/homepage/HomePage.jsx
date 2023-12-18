8c3e6b35d1e57a68e53305be868410ed00649e65
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-19 01:55:00 +0800

    Added itemdisplay header and see more button

--- src/src/homepage/HomePage.jsx
+++ src/src/homepage/HomePage.jsx
@@ -1,21 +1,34 @@
+import { useNavigate } from 'react-router-dom' 
 import ItemsDisplay from '../itemsdisplay/ItemsDisplay'
 import PromoBanners from './PromoBanners'
 import Categories from './Categories'
 import './homepage.css'
 
 export default function App() {
+  const navigate = useNavigate()
+
+  function handleSeeMore() {
+    navigate('/results?keyword=')
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
 
   return (
     <>
-      <div className=''>
-        <PromoBanners />
-        <Categories />
-        <div className='flex-row justify-center'>
+      <div className='homepage'>
+        <section className='promotion-section box-shadow'>
+          <PromoBanners />
+          <Categories />
+        </section>
+        <section className='items-section flex-column align-center'>
+          <header className='box-shadow text-center'>New Additions</header>
           <div className='items-display'>
             <ItemsDisplay searchAPI='/api/v1/search?keyword=' />
           </div>
-
-        </div>
+          <button className='see-more-button box-shadow' onClick={handleSeeMore}>See more</button>
+        </section>
       </div>
     </>
   )
