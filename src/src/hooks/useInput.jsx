67f210c101dc7eb4f0d0c95402b9427ad11a99b9
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2023-12-22 22:22:15 +0800

    Updated input custom hook css

--- src/src/hooks/useInput.jsx
+++ src/src/hooks/useInput.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
+import './input.css'
 
 function useInput(type, placeholder = '') {
   const [value, setValue] = useState('')
-  const input = <input
-                  type={type}
-                  onChange={e => setValue(e.target.value)}
-                  value={value}
-                  placeholder={placeholder}
-                />
-  return {value, input, setValue}
+  const input = (
+    <div className='input-wrapper'>
+      <input className='input'
+        type={type} 
+        onChange={e => setValue(e.target.value)}
+        placeholder=''
+        data-placeholder={placeholder}
+        value={value}
+        required
+      />
+      <span className='placeholder'>{placeholder}</span>
+    </div>
+  )
+  return { value, input, setValue }
 }
 
 export default useInput
