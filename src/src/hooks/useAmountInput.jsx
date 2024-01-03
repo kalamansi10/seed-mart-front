d76c06270feb895c1d45a6e7aa1916b0f15f6e17
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-04 06:59:27 +0800

    Minor css and bug fix

--- src/src/hooks/useAmountInput.jsx
+++ src/src/hooks/useAmountInput.jsx
@@ -6,33 +6,36 @@ export default function useAmountInput(min, max) {
   const input = () => {
     return (
       <div className='amount-input flex-row'>
-        <button onClick={() => handleClickAmount(-1)}>-</button>
+        <button onClick={() => handleClick(-1)}>-</button>
         <input
           type='text'
           name='minimum'
-          onChange={handlePriceFilterChange}
+          onChange={handleChange}
+          onBlur={handleBlur}
           value={value}
         />
-        <button onClick={() => handleClickAmount(1)}>+</button>
+        <button onClick={() => handleClick(1)}>+</button>
       </div>
-    )  
+    )
   }
 
-  function handleClickAmount(adjustment) {
+  function handleClick(adjustment) {
     setValue(prev => {
       let result = adjustment + prev
       return result > min && result < max ? result : prev
     })
   }
 
-  function handlePriceFilterChange(e) {
-    let value = e.target.value
-    if (value == '') {
-      setValue(1)
-    } else if (Number(value) && value.length < 4) {
-      setValue(Number(value))
+  function handleChange(e) {
+    let value = Number(e.target.value)
+    if (value >= min && value < max) {
+      setValue(value)
     }
   }
 
-  return {value, setValue, input}
+  function handleBlur() {
+    if (value == '') setValue(1)
+  }
+
+  return { value, setValue, input }
 }
