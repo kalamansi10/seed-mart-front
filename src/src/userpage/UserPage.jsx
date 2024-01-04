bdc2634657cb2a5c6c69c525c297e1fc9845eded
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-05 02:10:05 +0800

    Added full-height class to main page container

--- src/src/userpage/UserPage.jsx
+++ src/src/userpage/UserPage.jsx
@@ -4,17 +4,21 @@ import './user-page.css'
 export default function UserPage({ currentUser }) {
 
   return (
-    <div className='user-page flex-row justify-center'>
-      <section className='user-options'>
-        <Link to='/user/profile'><p>Profile</p></Link>
-        <Link to='/user/addresses'><p>Addresses</p></Link>
-        <Link to='/user/payment-methods'><p>Payment Methods</p></Link>
-        <Link to='/user/orders'><p>Orders</p></Link>
-        <Link to='/user/reviews'><p>Reviews</p></Link>
-      </section>
-      <section className='options-section box-shadow'>
-        <Outlet />
-      </section>
-    </div>
+    <>
+      <div className='full-height flex-column'>
+        <div className='user-page flex-row justify-center'>
+          <section className='user-options'>
+            <Link to='/user/profile'><p>Profile</p></Link>
+            <Link to='/user/addresses'><p>Addresses</p></Link>
+            <Link to='/user/payment-methods'><p>Payment Methods</p></Link>
+            <Link to='/user/orders'><p>Orders</p></Link>
+            <Link to='/user/reviews'><p>Reviews</p></Link>
+          </section>
+          <section className='options-section box-shadow'>
+            <Outlet />
+          </section>
+        </div>
+      </div>
+    </>
   )
 }
