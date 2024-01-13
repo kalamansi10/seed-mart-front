9ac6a2df65917ed74d8baaadf4396a5cc3d60268
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-01-13 15:41:02 +0800

    Added dialog for checkout processing and status

--- src/src/dialogs/CheckoutDialog.jsx
+++ src/src/dialogs/CheckoutDialog.jsx
@@ -1,10 +1,48 @@
-import { useState, useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import './check-out-dialog.css'
 
-export default function CheckoutDialog({checkoutDialog}) {
+export default function CheckoutDialog({ checkoutDialog, referenceNumber }) {
+  // Hook for programmatic navigation
+  const navigate = useNavigate()
+
+  function handleButtonClick() {
+    window.scrollTo({ top: 100, left: 100, behavior: 'smooth' })
+    navigate('/')
+  }
+
+  function renderStatus() {
+    if (referenceNumber) {
+      return (
+        <>
+          <div className="success-checkmark">
+            <div className="check-icon">
+              <span className="icon-line line-tip"></span>
+              <span className="icon-line line-long"></span>
+              <div className="icon-circle"></div>
+              <div className="icon-fix"></div>
+            </div>
+          </div>
+          <p className='order-status-label'>Your order is being shipped.</p>
+          <p className='reference-number'>Ref: {referenceNumber}</p>
+          <button onClick={handleButtonClick}>Order Summary</button>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <div className="loading-indicator"></div>
+          <p className='order-status-label'>Processing order...</p>
+        </>
+      )
+    }
+  }
+
 
   return (
-    <dialog ref={checkoutDialog.ref}>
-      
+    <dialog className='check-out-dialog box-shadow' ref={checkoutDialog.ref}>
+      <div className='flex-column align-center'>
+        {renderStatus()}
+      </div>
     </dialog>
   )
 }
