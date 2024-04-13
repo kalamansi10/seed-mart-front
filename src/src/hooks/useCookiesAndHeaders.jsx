2f0ce35ccf80a9af939012aaa3c546b3ca1e7ca0
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-13 14:30:50 +0800

    Updated useGetCookie to also generate fetch headers

---
+++ src/src/hooks/useCookiesAndHeaders.jsx
@@ -0,0 +1,25 @@
+function useCookiesAndHeaders() {
+  const getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    console.log(value);
+    const parts = value.split(`; ${name}=`);
+    console.log(parts);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  };
+
+  const getHeader = (method, body={}) => {
+    return {
+      method: method,
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": getCookie("CSRF-TOKEN"),
+      },
+      body: JSON.stringify(body),
+    };
+  };
+
+  return { getCookie, getHeader };
+}
+
+export default useCookiesAndHeaders;
