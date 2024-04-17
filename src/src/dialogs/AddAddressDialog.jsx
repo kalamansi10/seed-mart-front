df10fa1b47a46a2ff121bab368ae82b5312ac9e2
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-17 18:42:17 +0800

    Refactored and added comments

--- src/src/dialogs/AddAddressDialog.jsx
+++ src/src/dialogs/AddAddressDialog.jsx
@@ -11,14 +11,19 @@ export default function AddAddressDialog({
 }) {
   const { getHeader } = useCookiesAndHeaders();
 
+  // Input states for submission
   const [regions, setRegions] = useState([]);
   const [provinces, setProvinces] = useState([]);
   const [cities, setCities] = useState([]);
   const [barangays, setBarangays] = useState([]);
+  const [isMainAddress, setIsMainAddress] = useState(true);
 
+  // Custom input input hook
   const contactName = useInput("text", "Contact name");
   const contactNumber = useInput("tel", "Contact number");
   const streetAddress = useInput("text", "Street address");
+
+  // Custom select input hook
   const regionSelect = useSelectOptions(
     regions.map((region) => region.name),
     "Region"
@@ -36,12 +41,12 @@ export default function AddAddressDialog({
     "Barangay"
   );
 
-  const [isMainAddress, setIsMainAddress] = useState(true);
-
+  // List regions
   useEffect(() => {
     fetchPSGC(setRegions, "regions");
   }, []);
 
+  // List provinces for selected region
   useEffect(() => {
     let selectedRegion = regions.find(
       (region) => region.name == regionSelect.value
@@ -55,6 +60,7 @@ export default function AddAddressDialog({
     }
   }, [regionSelect.value]);
 
+  // List cities for selected province
   useEffect(() => {
     let selectedProvince = provinces.find(
       (province) => province.name == provinceSelect.value
@@ -68,53 +74,84 @@ export default function AddAddressDialog({
     }
   }, [provinceSelect.value]);
 
+  // List barangays for selected city
   useEffect(() => {
     let selectedCity = cities.find((city) => city.name == citySelect.value);
     if (!selectedCity) return;
     fetchPSGC(setBarangays, "/cities/" + selectedCity.code + "/barangays");
   }, [citySelect.value]);
 
+  // Form fill for updating existing address
   useEffect(() => {
-    contactName.setValue(updatedAddress.contact_name || "");
-    contactNumber.setValue(updatedAddress.contact_number || "");
-    streetAddress.setValue(updatedAddress.street_address || "");
-    barangaySelect.setValue(updatedAddress.barangay || "");
-    citySelect.setValue(updatedAddress.city || "");
-    provinceSelect.setValue(updatedAddress.province || "");
-    regionSelect.setValue(updatedAddress.region || "");
-    setIsMainAddress(updatedAddress.is_main || true);
+    const {
+      contact_name,
+      contact_number,
+      street_address,
+      barangay,
+      city,
+      province,
+      region,
+      is_main,
+    } = updatedAddress || {};
+
+    contactName.setValue(contact_name || "");
+    contactNumber.setValue(contact_number || "");
+    streetAddress.setValue(street_address || "");
+    barangaySelect.setValue(barangay || "");
+    citySelect.setValue(city || "");
+    provinceSelect.setValue(province || "");
+    regionSelect.setValue(region || "");
+    setIsMainAddress(is_main || true);
   }, [updatedAddress]);
 
-  function fetchPSGC(setState, endpoint) {
-    fetch(`https://psgc.gitlab.io/api/${endpoint}`)
-      .then((response) => response.json())
-      .then((data) => setState(data));
+  // PSGC API
+  async function fetchPSGC(setState, endpoint) {
+    try {
+      const response = await fetch(`https://psgc.gitlab.io/api/${endpoint}`);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+      setState(data);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
-  function handleAddressForm(e) {
-    let httpMethod = updatedAddress.id ? "PUT" : "POST";
-    let endPoint = updatedAddress.id ? "update-shipping-address" : "add-shipping-address";
-
+  // Address submit action
+  async function handleAddressForm(e) {
     e.preventDefault();
 
-    fetch(
+    const httpMethod = updatedAddress.id ? "PUT" : "POST";
+    const endPoint = updatedAddress.id
+      ? "update-shipping-address"
+      : "add-shipping-address";
+
+    const addressBody = {
+      contact_name: contactName.value,
+      contact_number: contactNumber.value,
+      street_address: streetAddress.value,
+      barangay: barangaySelect.value,
+      city: citySelect.value,
+      province: provinceSelect.value,
+      region: regionSelect.value,
+      is_main: isMainAddress,
+      ...(updatedAddress.id && { id: updatedAddress.id }), // Add id if exists
+    };
+
+    const response = await fetch(
       `/api/v1/${endPoint}`,
-      getHeader(httpMethod, {
-        shipping_address: {
-          contact_name: contactName.value,
-          contact_number: contactNumber.value,
-          street_address: streetAddress.value,
-          barangay: barangaySelect.value,
-          city: citySelect.value,
-          province: provinceSelect.value,
-          region: regionSelect.value,
-          is_main: isMainAddress,
-        },
-      })
-    ).then(() => {
-      fetchShippingAddresses();
+      getHeader(httpMethod, { shipping_address: addressBody })
+    );
+
+    if (response.ok) {
+      await fetchShippingAddresses();
       addAddressDialog.close();
-    });
+    } else {
+      console.error("Failed to fetch data");
+    }
   }
 
   return (
