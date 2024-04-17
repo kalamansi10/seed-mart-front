800661227335e8b38559dfde6623954dc8735ff7
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-17 16:51:23 +0800

    Removed logger

--- src/src/hooks/useCookiesAndHeaders.jsx
+++ src/src/hooks/useCookiesAndHeaders.jsx
@@ -1,9 +1,7 @@
 function useCookiesAndHeaders() {
   const getCookie = (name) => {
     const value = `; ${document.cookie}`;
-    console.log(value);
     const parts = value.split(`; ${name}=`);
-    console.log(parts);
     if (parts.length === 2) return parts.pop().split(";").shift();
   };
 
