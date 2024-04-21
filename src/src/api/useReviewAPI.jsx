758abfdcd30456fa21814e673983e4bf11dd0adc
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-21 17:02:13 +0800

    Moved renderRatings to review API

--- src/src/api/useReviewAPI.jsx
+++ src/src/api/useReviewAPI.jsx
@@ -1,4 +1,6 @@
 import useCookiesAndHeaders from "../hooks/useCookiesAndHeaders";
+import emptyStar from "../../assets/empty-star.svg";
+import filledStar from "../../assets/filled-star.svg";
 
 export default function useReviewAPI() {
   const { getHeader } = useCookiesAndHeaders();
@@ -54,11 +56,56 @@ export default function useReviewAPI() {
     }
   }
 
+  function renderStarRatings(rating, size) {
+    let starSize = ""
+    
+    switch(size) {
+      case "small":
+        starSize = "10px"
+        break;
+      default:
+        starSize = "17px"
+    }
+    
+    if (rating == 0) return <span>No ratings yet</span>;
+    let result = [];
+    let gray = 5 - rating;
+    for (let i = 0; i < rating; i++) {
+      result.push(
+        <img
+          key={i + "yellow"}
+          className="filled-star"
+          src={filledStar}
+          alt="yellow"
+          style={{width: starSize, height: starSize}}
+        />
+      );
+    }
+    for (let i = 0; i < gray; i++) {
+      result.push(
+        <img
+          key={i + "gray"}
+          className="empty-star"
+          src={emptyStar}
+          alt="gray"
+          style={{width: starSize, height: starSize}}
+        />
+      );
+    }
+    return (
+      <>
+        <span className="rating">{rating}</span>&nbsp;&nbsp;
+        <span>{result}</span>
+      </>
+    );
+  }
+
   return {
     getReviewList,
     getReview,
     addReview,
     editReview,
     deleteReview,
+    renderStarRatings,
   };
 }
