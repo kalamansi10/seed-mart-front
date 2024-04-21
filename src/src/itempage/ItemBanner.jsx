c394416012648e9287b26b7c9bc80799c2c6ba71
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-21 17:02:42 +0800

    Reconfigured review star ratings

--- src/src/itempage/ItemBanner.jsx
+++ src/src/itempage/ItemBanner.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import AddToCart from "./AddToCart";
 import useItemProps from "../hooks/useItemProps";
 import useAmountInput from "../hooks/useAmountInput";
-import emptyStar from "../../assets/empty-star.svg";
-import filledStar from "../../assets/filled-star.svg";
 
-export default function ItemBanner({ item, createPopUp }) {
+export default function ItemBanner({
+  item,
+  itemReviews,
+  renderStarRatings,
+  createPopUp,
+}) {
   const itemAmount = useAmountInput(0, 9999);
   const [list, listFields] = useItemProps();
 
@@ -29,36 +32,15 @@ export default function ItemBanner({ item, createPopUp }) {
     return result.slice(0, -1);
   }
 
-  function renderItemRating(item) {
-    let rating = item.average_rating;
-    if (rating == 0) return <span>No ratings yet</span>;
-    let result = [];
-    let gray = 5 - rating;
-    for (let i = 0; i < rating; i++) {
-      result.push(
-        <img className="filled-star" src={filledStar} alt="yellow" />
-      );
-    }
-    for (let i = 0; i < gray; i++) {
-      result.push(<img className="empty-star" src={emptyStar} alt="gray" />);
-    }
-    return (
-      <>
-        <span className="rating">{rating}</span>&nbsp;&nbsp;
-        <span>{result}</span>
-        &nbsp;&nbsp;|&nbsp;&nbsp;
-        <span className="ratings-count">{item.reviews.length}</span>&nbsp;
-        <span>ratings</span>
-      </>
-    );
-  }
-
   if (list) {
     return (
       <div className="description-container flex-column">
         <p className="item-name-banner">{item.name}</p>
         <div className="rating-sold-container flex-row align-center">
-          {renderItemRating(item)}
+          {renderStarRatings(item.average_rating)}
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <span className="ratings-count">{itemReviews && itemReviews.length}</span>&nbsp;
+          <span>ratings</span>
           &nbsp;&nbsp;|&nbsp;&nbsp;
           <span className="item-sold">{item.item_sold}</span>&nbsp;
           <span>{" sold"}</span>
