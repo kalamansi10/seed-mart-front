5622263a6e53aa753b99bbd460182eabd78e1fc3
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-21 13:51:06 +0800

    User addresses section initial styles

--- src/src/userpage/Addresses.jsx
+++ src/src/userpage/Addresses.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useAccountAPI from "../api/useAccountAPI";
 import AddAddressDialog from "../dialogs/AddAddressDialog";
 import useDialog from "../hooks/useDialog";
+import "./addresses.css";
 
 export default function Addresses({ currentUser }) {
   const [renderAddresses, setRenderAddresses] = useState();
@@ -22,39 +23,70 @@ export default function Addresses({ currentUser }) {
 
   function mapShippingAddresses(shippingAddresses) {
     return shippingAddresses.map((shippingAddress) => (
-      <div key={shippingAddress.id}>
-        {`${shippingAddress.street_address}, ${shippingAddress.barangay}, ${shippingAddress.city}, ${shippingAddress.province}, ${shippingAddress.region}, ${shippingAddress.is_main}`}
-        <button
-          onClick={() => {
-            setUpdatedAddress(shippingAddress);
-            addAddressDialog.show();
-          }}
+      <>
+        <div
+          key={shippingAddress.id}
+          className="flex-row justify-between align-center"
         >
-          Edit
-        </button>
-        <button
-          onClick={async () => {
-            removeShippingAddress(shippingAddress.id);
-            await fetchShippingAddressList();
-          }}
-        >
-          Delete
-        </button>
-      </div>
+          <div>
+            <div className="flex-row">
+              <div className="address-contact-name">
+                {shippingAddress.contact_name}
+              </div>
+              <div className="address-contact-number">
+                {shippingAddress.contact_number}
+              </div>
+            </div>
+            <div className="shipping-address">
+              {shippingAddress.street_address}
+            </div>
+            <div className="shipping-address">{`${shippingAddress.barangay}, ${shippingAddress.city}, ${shippingAddress.province}, ${shippingAddress.region}`}</div>
+            {shippingAddress.is_main && <div className="shipping-address">Default</div>}
+          </div>
+          <div>
+            <div className="address-options flex-row justify-center">
+              <button
+                className="address-edit"
+                onClick={() => {
+                  setUpdatedAddress(shippingAddress);
+                  addAddressDialog.show();
+                }}
+              >
+                Edit
+              </button>
+              <button
+                className="address-delete"
+                onClick={async () => {
+                  removeShippingAddress(shippingAddress.id);
+                  await fetchShippingAddressList();
+                }}
+              >
+                Delete
+              </button>
+            </div>
+            <button className={`default-address ${shippingAddress.is_main && ("is-default")}`}>Set as default</button>
+          </div>
+        </div>
+        <br />
+      </>
     ));
   }
 
   return (
-    <div>
+    <div className="addresses-section">
+      <div className="flex-row justify-between align-center">
+        <h2>ADDRESSES</h2>
+        <button
+          className="add-address-button"
+          onClick={() => {
+            setUpdatedAddress({});
+            addAddressDialog.show();
+          }}
+        >
+          Add new address
+        </button>
+      </div>
       {renderAddresses}
-      <button
-        onClick={() => {
-          setUpdatedAddress({});
-          addAddressDialog.show();
-        }}
-      >
-        New Address
-      </button>
       <AddAddressDialog
         addAddressDialog={addAddressDialog}
         updatedAddress={updatedAddress}
