814554ed262eced20f8801366c4f2b0b87714098
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-25 17:48:52 +0800

    Removed unused Link import

--- src/src/cartpage/CartPage.jsx
+++ src/src/cartpage/CartPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useListState from "../hooks/useListState";
 import useCartAPI from "../api/useCartAPI";
 import "./cart-page.css";
