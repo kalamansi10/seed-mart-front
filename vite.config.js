96f1059b6570cb747afd91f6449cbdff8255b173
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-04-26 18:47:18 +0800

    Configured backend url as env variable

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+        },
       },
-      "/users": "http://localhost:3000",
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
